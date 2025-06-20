d9999c899dadabefcd698bf47c9c52bee59b4968
hoodcm <cmichaelhood@gmail.com>
2025-06-19 20:19:57 -0400

    styling

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ["ui-monospace", "SFMono-Regular", "Menlo", "monospace"],
+      },
+    },
+  },
+  corePlugins: {
+    preflight: false   // disable base reset to preserve brutalist rawness
+  },
+  plugins: [],
+};
