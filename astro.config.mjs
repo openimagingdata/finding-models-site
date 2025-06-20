4372a5455f3ddb1ff80760435a2582ac9af09f35
hoodcm <cmichaelhood@gmail.com>
2025-06-19 21:16:16 -0400

    comprehensive update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   base: '/finding-models-site/',
+  // Remove `integrations`
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
